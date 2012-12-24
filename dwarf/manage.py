0de4a63d203d9431d09a3567b758e30d7af3caed
Xabier Larrakoetxea <slok69@gmail.com>
2012-12-24 11:44:26 +0100

    update the form of loading settings

--- dwarf/manage.py
+++ dwarf/manage.py
@@ -2,8 +2,29 @@
 import os
 import sys
 
+# The settings filenames without ".py" extension
+SETTINGS = {
+    'production': "prod",
+    'development': "dev",
+    'continous_integration': "ci",
+}
+
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dwarf.settings.dev")
+
+    # Settings import format
+    settings_location = "dwarf.settings.{0}"
+
+    # Check wich is the environment for the settings
+    if os.getenv('CI') == "true" or os.getenv('TRAVIS') == "true":
+        active_settings = SETTINGS['continous_integration']
+    elif os.getenv('PRODUCTION') == "true":
+        active_settings = SETTINGS['production']
+    else:
+        active_settings = SETTINGS['development']
+
+    # Load settings
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE",
+                        settings_location.format(active_settings))
 
     from django.core.management import execute_from_command_line
 
