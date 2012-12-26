5ea40bf5e29bdc12814687778566da4ce7537d33
Xabier Larrakoetxea <slok69@gmail.com>
2012-12-26 17:01:12 +0100

    Added json serialization for the template graphs

--- dwarf/simple/views.py
+++ dwarf/simple/views.py
@@ -1,3 +1,5 @@
+import json
+
 from django.shortcuts import (render_to_response,
                              RequestContext)
 from django.http import Http404
@@ -44,12 +46,45 @@ def index(request):
 
 
 def details(request, token):
-    print("Hello {0}".format(token))
+
+    clicks = Click.findall(token=token)
+    short_link = ShortLink.find(token=token),
+
+    # Get data for the charts
+    browsers = get_data_for_charts(clicks, "browser")
+    browsers.append("Browsers")
+    os = get_data_for_charts(clicks, "os")
+    os.append("OS")
+    country = get_data_for_charts(clicks, "location")
+    country.append("Country")
+
+    not_json_data = (browsers, os, country)
+
+    #Serialize
+    json_data = json.dumps(not_json_data)
+
     data = {
-        'shortlink': ShortLink.find(token=token),
-        'clicks': Click.findall(token=token)
+        'shortlink': short_link,
+        'clicks': clicks,
+        'json_data': json_data
     }
 
     return render_to_response('simple/details.html',
                             data,
                             context_instance=RequestContext(request))
+
+
+def get_data_for_charts(clicks, param):
+    """ Returns a tuple with (titles, counts) of a given parameter to extract
+    from the click inforamtion
+    """
+
+    my_list_aux = [getattr(i, param, "Unknown") for i in clicks]
+    my_list = []
+    for i in my_list_aux:
+        x = "Unknown" if not i else i
+        my_list.append(x)
+    titles = set(my_list)
+    counts = [my_list.count(i) for i in titles]
+    print(my_list)
+    return [list(titles), counts]
