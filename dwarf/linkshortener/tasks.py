39e5411f72da10c31cee682f6cab51d3154734c9
Xabier Larrakoetxea <slok69@gmail.com>
2013-03-16 19:34:40 +0100

    Get title when request a token. Closes #3

--- dwarf/linkshortener/tasks.py
+++ dwarf/linkshortener/tasks.py
@@ -2,6 +2,7 @@ from celery import task
 from django.contrib.auth.models import User
 
 from linkshortener.models import ShortLink, UserLink
+from dwarfutils.urlutils import extract_url_title
 
 
 @task()
@@ -9,10 +10,18 @@ def create_token(url, user_id=None):
     # Get the next counter to create the token
     counter = ShortLink.incr_counter()
 
+    # Get the title
+    # Fix this!! test if the url exists or not!!
+    try:
+        title = extract_url_title(url)
+    except:
+        title = "No title"
+
     # Create the instance with the data
     sl = ShortLink()
     sl.counter = counter
     sl.url = url
+    sl.title = title
 
     # Save
     sl.save()
@@ -23,7 +32,6 @@ def create_token(url, user_id=None):
         user_link.user = User.objects.get(id=user_id)
         user_link.token = sl.token
         user_link.save()
-
 
     # Return the new token
     return sl.token
