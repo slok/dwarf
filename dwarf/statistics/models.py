19646bf1c181fbecb888f99ffab790d4b88f4d76
Xabier Larrakoetxea <slok69@gmail.com>
2013-03-18 22:55:08 +0100

    Added basic functionality to bitmap statistics

--- dwarf/statistics/models.py
+++ dwarf/statistics/models.py
@@ -1,3 +1,99 @@
-from django.db import models
+from dwarfutils.redisutils import get_redis_connection
 
-# Create your models here.
+
+class BitmapStatistics(object):
+    """ This class is an abstract class that represents a redis bitmap for
+    statistics.
+
+    The statistics are based on 1 and 0 values along a bitmap, each position
+    of the bitmap represents something. Anexample could be:
+        * position 1 is the user id 1, position 2 is the user id 2...
+        * if the flag is up (1) means that the user has logged
+        * if the flag is down (0) means tha the user hasn't logged in
+    With this example we could set the users that logged in an specific moment
+    for example each day of the month.
+
+    To use this class, we only have to implement the way that the key is setted.
+    for example in the constructor or in the setter of an attribute. The only
+    important thing is that the class needs to know the key (self._key) at the
+    moment where python talks to Redis, (save or retrieve)
+
+    """
+
+    FLAG_UP = 1
+    FLAG_DOWN = 0
+
+    def __init__(self, bitmap=None):
+        self._bitmap = bitmap
+        self._key = None
+
+    @property
+    def key(self):
+        return self._key
+
+    def set_flag(self, flag_position):
+        r = get_redis_connection()
+        r.setbit(self._key, flag_position, BitmapStatistics.FLAG_UP)
+
+    def unset_flag(self, flag_position):
+        r = get_redis_connection()
+        r.setbit(self._key, flag_position, BitmapStatistics.FLAG_DOWN)
+
+    def set_flags(self, flag_positions):
+        r = get_redis_connection()
+        pipe = r.pipeline()
+        for i in flag_positions:
+            pipe.setbit(self._key, i, BitmapStatistics.FLAG_UP)
+        pipe.execute()
+
+    def unset_flags(self, flag_positions):
+        r = get_redis_connection()
+        pipe = r.pipeline()
+        for i in flag_positions:
+            pipe.setbit(self._key, i, BitmapStatistics.FLAG_DOWN)
+        pipe.execute()
+
+    def get_flag(self, flag_position):
+        r = get_redis_connection()
+        return r.getbit(self._key, flag_position)
+
+    def get_flags(self, flag_positions):
+        r = get_redis_connection()
+        pipe = r.pipeline()
+        for i in flag_positions:
+            pipe.getbit(self._key, i)
+        return pipe.execute()
+
+
+class LoginStatistics(BitmapStatistics):
+    """ Bitmap statistics for  the users that have been logged in a certain
+    moment in time.
+
+        * bitmap flag 1 represents that a user has logged in
+        * bitmap flag 0 represents that a user hasen't logged yet
+        * the position of the bitmap represents the user id
+    """
+
+    STATISTICS_KEY = "Statistics:login:{0}"  # Use Standard ISO-8601
+    DATE_FORMAT = "%Y-%m-%dT%H:%M"
+
+    def __init__(self, statistics_date=None, bitmap=None):
+        super(LoginStatistics, self).__init__(bitmap=bitmap)
+
+        self._statistics_date = statistics_date
+        if self._statistics_date:
+            time = self._statistics_date.strftime(LoginStatistics.DATE_FORMAT)
+            key = LoginStatistics.STATISTICS_KEY.format(time)
+        else:
+            key = None
+        self._key = key
+
+    @property
+    def statistics_date(self):
+        return self._statistics_date
+
+    @statistics_date.setter
+    def statistics_date(self, value):
+        self._statistics_date = value
+        time = self._statistics_date.strftime(LoginStatistics.DATE_FORMAT)
+        self._key = LoginStatistics.STATISTICS_KEY.format(time)
--- dwarf/statistics/tests.py
+++ dwarf/statistics/tests.py
@@ -1,16 +1,145 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
+import random
 
 from django.test import TestCase
+from datetime import datetime
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+from dwarfutils.redisutils import get_redis_connection
+from statistics.models import LoginStatistics
+
+
+class LoginStatisticsTest(TestCase):
+    def setUp(self):
+        self.year = 2013
+        self.month = "03"
+        self.day = 18
+        self.hour = 20
+        self.minutes = "00"
+        self.date = datetime(year=self.year,
+                             month=int(self.month),
+                             day=self.day,
+                             hour=self.hour,
+                             minute=int(self.minutes))
+
+        self.r = get_redis_connection()
+
+    def tearDown(self):
+        r = get_redis_connection()
+        r.flushdb()
+
+    def test_key_generation(self):
+        strtime = "{0}-{1}-{2}T{3}:{4}".format(self.year,
+                                               self.month,
+                                               self.day,
+                                               self.hour,
+                                               self.minutes)
+        good_key = LoginStatistics.STATISTICS_KEY.format(strtime)
+
+        ls = LoginStatistics(self.date)
+        self.assertEquals(good_key, ls.key)
+
+    def test_login_user(self):
+        user_id = random.randrange(0, 100000)
+        ls = LoginStatistics(self.date)
+
+        # Initial State
+        bit = self.r.getbit(ls.key, user_id)
+        self.assertEquals(LoginStatistics.FLAG_DOWN, bit)
+
+        # After login
+        ls.set_flag(user_id)
+        bit = self.r.getbit(ls.key, user_id)
+        self.assertEquals(LoginStatistics.FLAG_UP, bit)
+
+    def test_login_users(self):
+        users_ids = [random.randrange(0, 100000) for i in range(100)]
+        ls = LoginStatistics(self.date)
+
+        # Initial State
+        for i in users_ids:
+            bit = self.r.getbit(ls.key, i)
+            self.assertEquals(LoginStatistics.FLAG_DOWN, bit)
+
+        # After login
+        ls.set_flags(users_ids)
+        for i in users_ids:
+            bit = self.r.getbit(ls.key, i)
+            self.assertEquals(LoginStatistics.FLAG_UP, bit)
+
+    def test_logout_user(self):
+        user_id = random.randrange(0, 100000)
+        ls = LoginStatistics(self.date)
+
+        # After login
+        ls.set_flag(user_id)
+        bit = self.r.getbit(ls.key, user_id)
+        self.assertEquals(LoginStatistics.FLAG_UP, bit)
+
+        # after logout State
+        ls.unset_flag(user_id)
+        bit = self.r.getbit(ls.key, user_id)
+        self.assertEquals(LoginStatistics.FLAG_DOWN, bit)
+
+    def test_logout_users(self):
+        users_ids = [random.randrange(0, 100000) for i in range(100)]
+        ls = LoginStatistics(self.date)
+
+        # After login
+        ls.set_flags(users_ids)
+        for i in users_ids:
+            bit = self.r.getbit(ls.key, i)
+            self.assertEquals(LoginStatistics.FLAG_UP, bit)
+
+        # Initial State
+        ls.unset_flags(users_ids)
+        for i in users_ids:
+            bit = self.r.getbit(ls.key, i)
+            self.assertEquals(LoginStatistics.FLAG_DOWN, bit)
+
+    def test_user_logged(self):
+        user_id = random.randrange(0, 100000)
+        ls = LoginStatistics(self.date)
+
+        # Initial State
+        self.assertEquals(LoginStatistics.FLAG_DOWN, ls.get_flag(user_id))
+
+        # After login
+        ls.set_flag(user_id)
+        self.assertEquals(LoginStatistics.FLAG_UP, ls.get_flag(user_id))
+
+    def test_users_logged(self):
+        users_ids = [random.randrange(0, 100000) for i in range(100)]
+        ls = LoginStatistics(self.date)
+
+        # Initial State
+        for i in users_ids:
+            self.assertEquals(LoginStatistics.FLAG_DOWN, ls.get_flag(i))
+
+        # After login
+        ls.set_flags(users_ids)
+        correct_list = [LoginStatistics.FLAG_UP for i in range(len(users_ids))]
+        self.assertEquals(correct_list, ls.get_flags(users_ids))
+
+    def test_users_state(self):
+        users_ids = [random.randrange(0, 100000) for i in range(100)]
+        ls = LoginStatistics(self.date)
+
+        # Initial State
+        for i in users_ids:
+            self.assertEquals(LoginStatistics.FLAG_DOWN, ls.get_flag(i))
+
+        # After login
+        ls.set_flags(users_ids)
+        correct_list = [LoginStatistics.FLAG_UP for i in range(len(users_ids))]
+        self.assertEquals(correct_list, ls.get_flags(users_ids))
+
+        # take some random users out
+        black_ships = [random.randrange(0, len(users_ids)) for i in range(
+                                                            len(users_ids))]
+
+        for i in black_ships:
+            ls.unset_flag(users_ids[i])
+            correct_list[i] = LoginStatistics.FLAG_DOWN
+
+        # Test again
+        self.assertEquals(correct_list, ls.get_flags(users_ids))
