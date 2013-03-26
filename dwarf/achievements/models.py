f6d1380477ede4f277b4f82360a330dbd89de578
Xabier Larrakoetxea <slok69@gmail.com>
2013-03-26 21:50:00 +0100

    Added achievement model

--- dwarf/achievements/models.py
+++ dwarf/achievements/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Achievement(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=1000)
+    points = models.IntegerField()
+    level = models.IntegerField()
+
+    def __unicode__(self):
+        return u"{0}".format(self.name)
