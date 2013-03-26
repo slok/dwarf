a454727aad1e05e2a36d6ec5865580b926d446ab
Xabier Larrakoetxea <slok69@gmail.com>
2013-03-26 22:36:01 +0100

    Add image to achievements

--- dwarf/achievements/models.py
+++ dwarf/achievements/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Achievement(models.Model):
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=1000)
+    image = models.CharField(max_length=100)
     points = models.IntegerField()
     level = models.IntegerField()
 
