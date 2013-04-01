89201cb09a721873c16040b0055efac7fe9f5a5d
Xabier Larrakoetxea <slok69@gmail.com>
2013-04-02 00:06:19 +0200

    Added achievement percent to the achievements page

--- dwarf/achievements/views.py
+++ dwarf/achievements/views.py
@@ -11,19 +11,29 @@ from django.contrib.auth.decorators import login_required
 
 from achievements.models import Achievement
 from achievements.signals.signals import user_signup
+from metrics.models import AchievementMetrics
 
 logger = logging.getLogger("dwarf")
 
 
 @login_required
 def list_achievements(request):
-    achievements = Achievement.objects.all()
+    achievements_tmp = Achievement.objects.all()
+    users = User.objects.all().count()
+    achievements = []
+
+    for i in achievements_tmp:
+        total = AchievementMetrics(i.id).total_users()
+        percent = total * 100 / users
+        achievements.append((i, percent))
+
     context = {
         'achievements': achievements,
     }
+
     # Send signal
-    user = User.objects.get(id=1)
-    user_signup.send(sender=user)
+    #user = User.objects.get(id=1)
+    #user_signup.send(sender=user)
 
     return render_to_response('achievements/achievements.html',
                               context,
