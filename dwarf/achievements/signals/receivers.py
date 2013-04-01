4201efd0be9c28565cf158903a962bc13db04b7a
Xabier Larrakoetxea <slok69@gmail.com>
2013-04-01 21:46:54 +0200

    Implemented padawan achievement

--- dwarf/achievements/signals/receivers.py
+++ dwarf/achievements/signals/receivers.py
@@ -1,7 +1,27 @@
+import logging
+
+from metrics.models import AchievementMetrics
+from achievements import achievementsids
+from achievements.models import Achievement, UserAchievement
+
+
+logger = logging.getLogger("dwarf")
 
 
 def padawan_achievement(sender, **kwargs):
-    print("Padawan achievement gained! {0}".format(sender.username))
+    achiv = Achievement.objects.get(id=achievementsids.PADAWAN)
+
+    # Save user achievement
+    user_achiv = UserAchievement()
+    user_achiv.achievement = achiv
+    user_achiv.user = sender
+    user_achiv.save()
+
+    # Save metrics
+    metrics = AchievementMetrics(achievementsids.PADAWAN)
+    metrics.add_user_achievement(sender.id)
+
+    logger.debug("Padawan achievement gained! {0}".format(sender.username))
 
 
 def login_achievement(sender, **kwargs):
--- dwarf/achievements/tests.py
+++ dwarf/achievements/tests.py
@@ -6,11 +6,34 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from django.contrib.auth.models import User
+
+from achievements.models import Achievement, UserAchievement
+from achievements.signals import receivers
+from achievements import achievementsids
+from metrics.models import AchievementMetrics
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class AchievementsTest(TestCase):
+    def test_padawan_achievement(self):
+        user = User()
+        user.username = "test"
+        user.save()
+
+        achiv = Achievement()
+        achiv.name = "padawan test"
+        achiv.id = achievementsids.PADAWAN
+        achiv.points = 0
+        achiv.level = 0
+        achiv.save()
+
+        receivers.padawan_achievement(user)
+
+        # Check
+        self.assertTrue(UserAchievement.objects.filter(
+                                                    user=user,
+                                                    achievement=achiv
+        ).exists())
+
+        metrics = AchievementMetrics(achievementsids.PADAWAN)
+        self.assertTrue(metrics.user_has_achievement(user.id))
