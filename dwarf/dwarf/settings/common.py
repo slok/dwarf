af51f6b667e7d6cb4e277a5d931009dcd18db472
Xabier Larrakoetxea <xabier@mydeco.com>
2013-04-03 12:23:01 +0100

    Added Django 1.5 missing settings

--- dwarf/dwarf/settings/common.py
+++ dwarf/dwarf/settings/common.py
@@ -48,6 +48,11 @@ DATABASES = {
 }
 #---------------------END DATABASE CONFIGURATION-------------------------------
 
+#-----------------------HOSTS CONFIGURATION------------------------------------
+# Hosts/domain names that are valid for this site; required if DEBUG is False
+# See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
+ALLOWED_HOSTS = []
+#-----------------------END HOSTS CONFIGURATION--------------------------------
 
 #-----------------------LOCALE CONFIGURATION-----------------------------------
 # Local time zone for this installation. Choices can be found here:
