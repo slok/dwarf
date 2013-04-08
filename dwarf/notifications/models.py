3c1ef351993e9fb626b41903b2609f16e2aa1f71
Xabier Larrakoetxea <slok69@gmail.com>
2013-04-08 12:32:16 +0100

    Added basic model for pushing notifications into redis. #26

--- dwarf/notifications/models.py
+++ dwarf/notifications/models.py
@@ -1,3 +1,65 @@
-from django.db import models
+import json
 
-# Create your models here.
+from dwarfutils.redisutils import get_redis_notifications_connection
+from dwarfutils.dateutils import unix_now_utc
+from achievements.templatetags.achievementfilters import achievement_image_url
+
+ACHIEVEMENT = 'achievement'
+LEVEL = 'level'
+
+
+class Notification(object):
+    KEY_FORMAT = "push:notifications:{0}"
+
+    def __init__(self,
+                 notification_type=None,
+                 title=None,
+                 description=None,
+                 image=None,
+                 date=None,
+                 user_id=None,
+                 key=None):
+        self._notification_type = notification_type
+        self._title = title
+        self._description = description
+        self._image = image
+        self._date = unix_now_utc()
+        self._user_id = user_id
+
+        if not key:
+            self._key = Notification.KEY_FORMAT.format(user_id)
+
+    def transform_json(self):
+        data = {
+            'type': self._notification_type,
+            'title': self._title,
+            'description': self._description,
+            'image': self._image,
+            'date': self._date
+        }
+        return json.dumps(data)
+
+    def save(self):
+        pass
+
+    def send_push(self):
+        # Publish in redis
+        r = get_redis_notifications_connection()
+        r.publish(self._key, self.transform_json())
+
+
+class AchievementNotification(Notification):
+
+    def __init__(self, achievement, user):
+        self._achievement_id = achievement.id
+        notification_type = ACHIEVEMENT
+        image = achievement_image_url(achievement.image)
+        title = "Achievement unlocked!"
+        description = "You gained the '{0}' achievement".format(achievement.name)
+        super(AchievementNotification, self).__init__(
+            notification_type=notification_type,
+            title=title,
+            description=description,
+            image=image,
+            user_id=user.id
+        )
--- dwarf/notifications/tests.py
+++ dwarf/notifications/tests.py
@@ -6,11 +6,18 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from django.contrib.auth.models import User
+
+from achievements.models import Achievement
+from notifications.models import AchievementNotification
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class AchievementNotificationTest(TestCase):
+    fixtures = ['achievement.json', 'user.json']
+
+    # Cant test because of the redis blocking
+    def test_send_push(self):
+        achieve = Achievement.objects.get(id=1)
+        user = User.objects.get(id=1)
+        notif = AchievementNotification(achievement=achieve, user=user)
+        notif.send_push()
