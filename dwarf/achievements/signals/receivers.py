478d84944f0a5be673a405dc29c2d3dff24bd23f
Xabier Larrakoetxea <slok69@gmail.com>
2013-04-09 15:00:37 +0100

    Add push and save notifications to the signal. closes #26

--- dwarf/achievements/signals/receivers.py
+++ dwarf/achievements/signals/receivers.py
@@ -1,8 +1,11 @@
 import logging
 
+from django.db import IntegrityError
+
 from metrics.models import AchievementMetrics
 from achievements import achievementsids
 from achievements.models import Achievement, UserAchievement
+from notifications.models import AchievementNotification
 
 
 logger = logging.getLogger("dwarf")
@@ -10,17 +13,25 @@ logger = logging.getLogger("dwarf")
 
 def save_achievement(user, achievement):
     """ Shortcut to save an achievement"""
-    # Save user achievement
-    user_achiv = UserAchievement()
-    user_achiv.achievement = achievement
-    user_achiv.user = user
-    user_achiv.save()
+    try:
+        # Save user achievement
+        user_achiv = UserAchievement()
+        user_achiv.achievement = achievement
+        user_achiv.user = user
+        user_achiv.save()
 
-    # Save metrics
-    metrics = AchievementMetrics(achievement.id)
-    metrics.add_user_achievement(user.id)
+        # Save metrics
+        metrics = AchievementMetrics(achievement.id)
+        metrics.add_user_achievement(user.id)
 
-    logger.debug("{0} gained '{1}'".format(user.username, achievement.name))
+        # Send notifications
+        notif = AchievementNotification(achievement, user=user)
+        notif.send_push()  # Push realtime notification
+        notif.save()  # save the notification for the dashboard
+
+        logger.debug("{0} gained '{1}'".format(user.username, achievement.name))
+    except IntegrityError:
+        logger.error("{0} already has '{1}'".format(user.username, achievement.name))
 
 
 def padawan_achievement(sender, **kwargs):
