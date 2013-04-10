ce7bc5bff832d7c76b68a1f2a50b10fa35346575
Xabier Larrakoetxea <slok69@gmail.com>
2013-04-10 21:04:31 +0200

    Added minor comment changes

--- dwarf/achievements/signals/register.py
+++ dwarf/achievements/signals/register.py
@@ -3,6 +3,6 @@ from django.contrib.auth.models import User
 from achievements.signals import receivers
 from achievements.signals import signals
 
-
+# Connect achievement to events
 signals.user_signup.connect(receivers.padawan_achievement)
 signals.user_login.connect(receivers.login_achievement, sender=User)
