b037d40251f29e33d21a077b5a2c513964cdd48d
Xabier Larrakoetxea <slok69@gmail.com>
2013-04-10 21:02:50 +0200

    Specified more triggers

--- dwarf/achievements/signals/signals.py
+++ dwarf/achievements/signals/signals.py
@@ -1,4 +1,17 @@
 import django.dispatch
 
+
+############## User events #################
+
+# Triggered when the user signs up
 user_signup = django.dispatch.Signal()
+
+# Triggered when the user logs in
 user_login = django.dispatch.Signal()
+
+############## Short link events #################
+# Triggered when the link is shortened
+link_shortened = django.dispatch.Signal()
+
+# Triggered whe a link is clicked
+link_clicked = django.dispatch.Signal()
