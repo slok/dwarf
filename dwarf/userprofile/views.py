68a26df8ed22b52693c1165d3b837f655603e962
Xabier Larrakoetxea <slok69@gmail.com>
2013-05-04 15:26:13 +0200

    Added dashboard info

--- dwarf/userprofile/views.py
+++ dwarf/userprofile/views.py
@@ -28,14 +28,16 @@ from userprofile.models import Profile
 from userprofile.forms import SignupForm, ResetPasswordForm
 from linkshortener.models import UserLink, ShortLink
 from dwarfutils.hashutils import get_random_hash
-from dwarfutils.dateutils import unix_to_datetime
 from metrics.models import LoginMetrics
 from achievements.signals.signals import user_signup
+from notifications.models import Notification
+from achievements.models import UserAchievement
 
 logger = logging.getLogger("dwarf")
 
-LINK_PER_PAGE = 10
-
+LINKS_IN_DASHBOARD = 5
+NOTIFS_IN_DASHBOARD = 10
+ACHIEV_IN_DASHBOARD = 4
 
 ################################################################################
 # Copied code from Django (Need to put code in between, like metrics and stuff)
@@ -197,50 +199,32 @@ def activate_account(request, user_id, token):
 @login_required
 def user_dashboard(request):
 
-    # get the page
-    page = int(request.GET.get('page', 1))
+    # Notifications
+    # Rest one beacause is the upper limit inclusive
+    notifs = Notification.find(user=request.user, limit=NOTIFS_IN_DASHBOARD-1)
 
-    # Get the total pages (rounding up, ex: 1.2 pages means 2 pages)
-    total_pages = int(math.ceil(
-                        float(UserLink.objects.filter(
-                                user=request.user).count()) / LINK_PER_PAGE))
+    # Stats
+    total_links = UserLink.objects.filter(user=request.user).count()
+    total_achieves = UserAchievement.objects.filter(user=request.user).count()
 
-    # If the page doesn't exists then 404
-    if page > total_pages and total_pages > 0:
-        raise Http404
-
-    # Get the links
-    offset = LINK_PER_PAGE * (page - 1)
-    limit = offset + LINK_PER_PAGE
-    links_aux = UserLink.objects.filter(user=request.user).order_by('-id')[offset:limit]
+    # Latest links
+    links_aux = UserLink.objects.filter(
+                        user=request.user).order_by('-id')[:LINKS_IN_DASHBOARD]
     links = [ShortLink.find(token=i.token) for i in links_aux]
 
-    # Group by day
-    grouped_links = []
-    temp = []
-
-    for i in links:
-        creation_date = unix_to_datetime(i.creation_date)
-
-        if len(temp) == 0:
-            temp.append(i)
-        else:
-            previous_date = unix_to_datetime(temp[0].creation_date)
-            if previous_date.year == creation_date.year and\
-                    previous_date.month == creation_date.month and\
-                    previous_date.day == creation_date.day:
-                temp.append(i)
-            else:
-                grouped_links.append(temp)
-                temp = []
-    # If no links don't add them
-    if temp:
-        grouped_links.append(temp)
-
+    # Latest achievements
+    achievements = UserAchievement.objects.filter(
+                        user=request.user).order_by('-id')[:ACHIEV_IN_DASHBOARD]
     context = {
-        "total_pages": total_pages,
-        "actual_page": page,
-        "links": grouped_links
+        "notifications": notifs,
+        "links": links,
+        "stats": {
+            "Total Links": total_links,
+            "Achievements": total_achieves,
+            "Level": 0,
+            "Points": 0,
+        },
+        "achievements": achievements
     }
 
     return render_to_response('userprofile/dashboard.html',
