07dad16cdffd6e51baa7db00f3165865da70747f
Xabier Larrakoetxea <slok69@gmail.com>
2013-05-04 10:37:11 +0200

    Added random url fill utility

---
+++ dwarf/dwarfutils/management/commands/fill_random_short_links.py
@@ -0,0 +1,46 @@
+import random
+
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+
+from linkshortener.tasks import create_token
+
+
+URLS = (
+    "http://www.google.com",
+    "http://www.github.com",
+    "http://www.python.org",
+    "http://mail.google.com",
+    "http://www.pogdesign.co.uk/cat/graph/",
+    "https://speakerdeck.com/slok",
+    "https://github.com/slok/dwarf",
+    "http://127.0.0.1:8000/notifications/",
+    "http://twitter.github.io/bootstrap/components.html#labels-badges",
+    "http://www.humblebundle.com/",
+    "https://twitter.com/sLoK69",
+    "http://grooveshark.com/#!/artist/Amon+Amarth/17733",
+    "https://news.ycombinator.com/",
+    "http://highscalability.com/",
+    "http://thepiratebay.se",
+    "https://docs.djangoproject.com/en/1.5/topics/db/managers/",
+    "http://flask.pocoo.org/",
+    "http://pygments.org/faq/#how-to-use",
+    "http://docs.vagrantup.com/v2/getting-started/up.html",
+    "http://ansible.cc/docs/patterns.html",
+)
+
+
+class Command(BaseCommand):
+    help = 'Fills random urls for all the users'
+
+    def handle(self, *args, **options):
+        users = User.objects.all()
+
+        # For every user
+        for user in users:
+            # How many ruls?
+            urls = set([URLS[random.randrange(0, len(URLS))] for i in range(random.randrange(0, len(URLS)))])
+            for url in urls:
+                print(url)
+                print(create_token(url, user.id))
+
