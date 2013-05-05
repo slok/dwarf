30a0d2f7e063e8a677bb6a0c4e60a34845609ee6
Xabier Larrakoetxea <slok69@gmail.com>
2013-05-05 12:51:01 +0200

    Changed form style for the link shortener

--- dwarf/linkshortener/forms.py
+++ dwarf/linkshortener/forms.py
@@ -3,4 +3,9 @@ from django.utils.translation import ugettext_lazy as _
 
 
 class ShortUrlForm(forms.Form):
-    url = forms.URLField(label=_(u'URL'))
+    url = forms.URLField(
+            label=_(u'URL'),
+            widget=forms.TextInput(attrs={
+                                    'placeholder': _(u'Short your Link!'),
+                                    'style': "width:220%",
+                                   }))
