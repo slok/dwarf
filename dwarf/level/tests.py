fb757898bb31d16b55b00b83d26eb94960cf599e
Xabier Larrakoetxea <slok69@gmail.com>
2013-05-05 15:21:24 +0200

    Added some utils for the points and the levels

--- dwarf/level/tests.py
+++ dwarf/level/tests.py
@@ -6,11 +6,102 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from django.contrib.auth.models import User
+
+from level import utils
+from level.models import Level
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class LevelUtilsTest(TestCase):
+    fixtures = ['levels.json', 'user.json']
+
+    def test_basic_increment(self):
+        incr_points = 50
+        user = User.objects.get(id=1)
+
+        self.assertEqual(0, user.profile.points)
+        self.assertEqual(0, user.profile.level.level_number)
+
+        level = utils.incr_points(user, incr_points)
+        self.assertEqual(incr_points, user.profile.points)
+        self.assertEqual(0, user.profile.level.level_number)
+        self.assertIsNone(level)
+
+    def test_basic_increment_without_level(self):
+        incr_points = 999
+        user = User.objects.get(id=1)
+
+        self.assertEqual(0, user.profile.points)
+        self.assertEqual(0, user.profile.level.level_number)
+
+        level = utils.incr_points(user, incr_points)
+        self.assertEqual(incr_points, user.profile.points)
+        self.assertEqual(0, user.profile.level.level_number)
+        self.assertIsNone(level)
+
+    def test_basic_increment_with_level(self):
+        incr_points = 5000
+        user = User.objects.get(id=1)
+
+        self.assertEqual(0, user.profile.points)
+        self.assertEqual(0, user.profile.level.level_number)
+
+        level = utils.incr_points(user, incr_points)
+        self.assertEqual(incr_points, user.profile.points)
+        self.assertEqual(5, user.profile.level.level_number)
+        self.assertEqual(5, level.level_number)
+
+    def test_basic_decrement(self):
+        decr_points = 50
+        user = User.objects.get(id=1)
+
+        self.assertEqual(0, user.profile.points)
+        self.assertEqual(0, user.profile.level.level_number)
+
+        level = utils.decr_points(user, decr_points)
+        self.assertEqual(0, user.profile.points)
+        self.assertEqual(0, user.profile.level.level_number)
+        self.assertIsNone(level)
+
+    def test_basic_decrement_without_level(self):
+        decr_points = 999
+        start_points = 1999
+
+        user = User.objects.get(id=1)
+        user.profile.points = start_points
+        user.profile.level = Level.objects.get(level_number=1)
+        user.save()
+
+        self.assertEqual(start_points, user.profile.points)
+
+        level = utils.decr_points(user, decr_points)
+        self.assertEqual(start_points - decr_points, user.profile.points)
+        self.assertEqual(1, user.profile.level.level_number)
+        self.assertIsNone(level)
+
+    def test_basic_decrement_with_level(self):
+        decr_points = 3000
+        start_points = 5000
+
+        user = User.objects.get(id=1)
+        user.profile.points = start_points
+        user.profile.level = Level.objects.get(level_number=5)
+        user.save()
+
+        self.assertEqual(start_points, user.profile.points)
+
+        level = utils.decr_points(user, decr_points)
+        self.assertEqual(start_points - decr_points, user.profile.points)
+        self.assertEqual(2, user.profile.level.level_number)
+        self.assertEqual(2, level.level_number)
+
+    def test_remainin_points_for_next_level(self):
+        points = 560
+        next_level_min = 1000
+
+        user = User.objects.get(id=1)
+        user.profile.points = points
+        user.save()
+
+        self.assertEqual(next_level_min-points,
+                         utils.points_for_next_level(user))
---
+++ dwarf/level/utils.py
@@ -0,0 +1,50 @@
+from level.models import Level
+
+
+def incr_points(user, points):
+    """Increments the points of an user, If the user has passed to a new level
+    it returns the level, if not then  returns None"""
+    points = user.profile.points + points
+
+    level = Level.objects.get(points_min__lte=points, points_max__gte=points)
+
+    # Has our user increase the level?
+    if user.profile.level.level_number < level.level_number:
+        user.profile.level = level
+    else:
+        level = None
+
+    user.profile.points = points
+    user.save()
+
+    return level
+
+
+def decr_points(user, points):
+    points = user.profile.points - points
+    if points < 0:
+        points = 0
+
+    level = Level.objects.get(points_min__lte=points, points_max__gte=points)
+
+    # Has our user decrease the level?
+    if user.profile.level.level_number > level.level_number:
+        user.profile.level = level
+    else:
+        level = None
+
+    user.profile.points = points
+    user.save()
+
+    return level
+
+
+def points_for_next_level(user):
+    points = user.profile.points
+    next_level = Level.objects.get(
+        level_number=(user.profile.level.level_number + 1))
+    return (next_level.points_min - points)
+
+
+def percent_for_next_level(user):
+    pass
