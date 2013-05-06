6ad4e5dc14613987acf305da3507def0b2ce4b98
Xabier Larrakoetxea <slok69@gmail.com>
2013-05-06 20:01:18 +0200

    added url for link detail

--- dwarf/links/urls.py
+++ dwarf/links/urls.py
@@ -6,4 +6,5 @@ import links.views
 urlpatterns = patterns('',
 
     url(r'^$', links.views.links_index, name="links-index"),
+    url(r'^info/(?P<token>\w+)$', links.views.links_info, name="links-info"),
 )
